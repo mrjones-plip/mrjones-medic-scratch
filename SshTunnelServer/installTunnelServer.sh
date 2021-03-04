a9df20fb6d151a28e82b15a931adec4d9f9bbda3
mrjones-plip <ashley@medicmobile.org>
2021-03-03 22:34:36 -0800

    break out ssl vs non-ssl hosts, output html file

--- SshTunnelServer/installTunnelServer.sh
+++ SshTunnelServer/installTunnelServer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# todo - put in a single loop?  fewer loops?
 # todo - check for Nth run to not create users that already exist etc
 # todo - save decent looking HTML instructions in $DOMAIN vhost
 # todo - maybe check for ubuntu?
@@ -53,9 +52,20 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 echo ""
-echo "This script assumes you're root and assumes you"
-echo "want to set a bunch of SSH tunnels to reverse proxy"
-echo "HTTPs and HTTP traffic."
+echo "This script assumes:"
+echo " "
+echo " - you're root on Ubuntu"
+echo " - you have a wildcard DNS entry pointed to this server for *.$DOMAIN"
+echo " - you want to set up a bunch of SSH tunnels to reverse proxy HTTPs "
+echo "   and HTTP traffic"
+echo " - are on on a machine dedicated to this purpose."
+echo ""
+echo "Will create accounts for these GitHub users using their"
+echo "SSH keys on GitHub:"
+echo ""
+for i in $(cat user.txt); do
+  echo " - $i"
+done
 echo ""
 echo "See https://github.com/mrjones-plip/mrjones-medic-scratch/tree/main/SshTunnelServer "
 echo "for more info"
@@ -67,16 +77,15 @@ read -n1 -s
 # install apache, rpl & certbot then enable mods
 # todo - uncomment this - this way to speed testing
 echo ""
-echo "Updating OS and installing required software, this might take a while..."
+echo " ------ Updating OS and installing required software, this might take a while... ------ "
 echo ""
 apt -qq update&&apt -y -qqq dist-upgrade&&apt -qqq install -y  apache2  rpl&&systemctl --now enable apache2
 sudo snap install core; sudo snap refresh core
 sudo snap install --classic certbot
-a2enmod proxy proxy_ajp proxy_http rewrite deflate headers proxy_balancer proxy_connect proxy_html
 
 # todo validate this works
 echo ""
-echo "Adding users..."
+echo " ------ Adding users... ------ "
 echo ""
 cp ./press_to_exit.sh /bin/press_to_exit.sh
 for i in $(cat user.txt); do
@@ -84,14 +93,15 @@ for i in $(cat user.txt); do
 done
 
 echo ""
-echo "Setting MOTD..."
+echo " ------ Setting MOTD... ------ "
 echo ""
 sudo chmod -x /etc/update-motd.d/*
 cp motd /etc/update-motd.d/02-ssh-tunnel-info
 sudo chmod +x /etc/update-motd.d/02-ssh-tunnel-info
 
-# create .ssh dir and authorized_keys with right perms and ownership
-# todo validate this works
+echo ""
+echo " ------ Adding SSH keys for users and setting file perms... ------ "
+echo ""
 for i in $(cat user.txt); do
   mkdir /home/$i/.ssh
   touch /home/$i/.ssh/authorized_keys
@@ -99,53 +109,106 @@ for i in $(cat user.txt); do
   chown $i:$i /home/$i/.ssh/authorized_keys
   chmod 700 /home/$i/.ssh
   chmod 600 /home/$i/.ssh/authorized_keys
-done
-
-# fetch .ssh keys into authorized keys file
-for i in $(cat user.txt); do
   curl -s https://github.com/$i.keys -o /home/$i/.ssh/authorized_keys
 done
 
-# add mrjones key to all to help testing (optional)
-for i in $(cat user.txt); do
-  curl -s https://github.com/mrjones-plip.keys >>/home/$i/.ssh/authorized_keys
-done
-
-#  create one file per user vhost and a custom port per file.
+echo ""
+echo "Adding apache vhost files..."
+echo ""
 for i in `cat user.txt`; do
-  cp ./apache.conf /etc/apache2/sites-available/$i.$DOMAIN.conf
-  rpl --encoding UTF-8  -q SUBDOMAIN $i /etc/apache2/sites-available/$i.$DOMAIN.conf
-  rpl --encoding UTF-8  -q DOMAIN $DOMAIN /etc/apache2/sites-available/$i.$DOMAIN.conf
   rand=`shuf -i1000-5000 -n1`
-  rpl --encoding UTF-8  -q PORT $rand /etc/apache2/sites-available/$i.$DOMAIN.conf
+  FQDNconf="{$i}.{$DOMAIN}.conf"
+  FQDN-ssl-conf="{$i}-ssl.{$DOMAIN}.conf"
+
+  cp ./apache.conf /etc/apache2/sites-available/$FQDNconf
+  cp ./apache.ssl.conf /etc/apache2/sites-available/$FQDN-ssl-conf
+
+  rpl --encoding UTF-8  -q SUBDOMAIN $i /etc/apache2/sites-available/$FQDNconf
+  rpl --encoding UTF-8  -q SUBDOMAIN $i /etc/apache2/sites-available/$FQDN-ssl-conf
+
+  rpl --encoding UTF-8  -q DOMAIN $DOMAIN /etc/apache2/sites-available/$FQDNconf
+  rpl --encoding UTF-8  -q DOMAIN $DOMAIN /etc/apache2/sites-available/$FQDN-ssl-conf
+
+  rpl --encoding UTF-8  -q PORT $rand /etc/apache2/sites-available/$FQDNconf
+  rpl --encoding UTF-8  -q PORT $rand /etc/apache2/sites-available/$FQDN-ssl-conf
+
+  a2ensite $FQDNconf $FQDN-ssl-conf
 done
 
-
-# enable vhosts
-for i in `cat user.txt`; do
-  a2ensite $i.$DOMAIN.conf
-done
-
-#enable certbot certs , change email
+echo ""
+echo " ------ Fetching certs from Let's Encrypt... ------ "
+echo ""
 for i in `cat user.txt`; do
   sudo certbot  --apache   --non-interactive   --agree-tos   --email $EMAIL --domains $i.$DOMAIN
 done
 
+echo ""
+echo " ------ Creating last cert for bare $DOMAIN domain... ------ "
+echo ""
 
 sudo certbot  --apache   --non-interactive   --agree-tos   --email $EMAIL --domains $DOMAIN
 
-# reload apache so new config takes effect
+echo ""
+echo " ------ Configuring and Reloading apache... ------ "
+echo ""
+a2enmod proxy proxy_ajp proxy_http rewrite deflate headers proxy_balancer proxy_connect proxy_html
 systemctl reload apache2
+MAPPING_NOSSL=$(grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*|grep -v '\-ssl\.'|cut -d/ -f5,8|cut -d: -f1,3|awk 'BEGIN{FS=OFS=":"}{print $2 FS  $1}'|sed -r 's/.conf//g'|sed -r 's/:/\t/g')
+MAPPING_SSL=$(grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*-ssl\.*|cut -d/ -f5,8|cut -d: -f1,3|awk 'BEGIN{FS=OFS=":"}{print $2 FS  $1}'|sed -r 's/.conf//g'|sed -r 's/:/\t/g')
+SAMPLE_PORT=$(grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*|cut -d/ -f5,8|cut -d: -f3|tail -n1)
+SAMPLE_HOST=$(grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*|cut -d/ -f5,8|cut -d: -f1|sed 's/.conf//g'|tail -n1)
+SAMPLE_LOGIN=$(grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*|cut -d/ -f5,8|cut -d: -f1|sed 's/.conf//g'|tail -n1|cut -d. -f1)
+
+echo "
+<style>
+* {
+	color: white;
+	background: black;
+}
+</style>
+<pre>
+ports for local <code>http</code> hosts:
+
+$MAPPING_NOSSL
+
+ports for local <code>https</code> hosts:
+
+$MAPPING_SSL
+
+use:
+
+     ssh -T -R PORT-FROM-ABOVE:127.0.0.1:PORT-ON-DEV GH-HANDLE@{$DOMAIN}
+
+example - expose your local port 80 on https://{$SAMPLE_HOST} using
+{$SAMPLE_LOGIN}'s GH keys:
+
+     ssh -T -R {$SAMPLE_PORT}:127.0.0.1:80 {$SAMPLE_LOGIN}@{$DOMAIN}
+
+note on  <code>http</code> vs <code>https</code> ports above:
+
+     Apache is configured on the <code>http</code> hosts to speak
+     <code>http</code> to your localhost server. Conversely, it is configured to speak
+     <code>https</code> on the <code>https</code> hosts. If you mix these up
+     it will try and speak <code>https</code> to <code>http</code> (or vise
+     versa) and it will fail.
+
+more info:
+
+     https://github.com/mrjones-plip/mrjones-medic-scratch/tree/main/SshTunnelServer
+</pre>
+" > /var/www/html/index.html
 
 echo ""
-echo "Outputting mapping..."
+echo " ------ Here's the final mapping for http: ------ "
 echo ""
-echo "-----------------"
-grep -ri -m1 'ProxyPassReverse' /etc/apache2/sites-available/*|cut -d/ -f5,8|cut -d: -f1,3|awk 'BEGIN{FS=OFS=":"}{print $2 FS  $1}'|sed -r 's/.conf//g'|sed -r 's/:/\t/g'
-echo "-----------------"
+echo $MAPPING_NOSSL
 echo ""
+echo " ------ Here's the final mapping for https: ------ "
+echo ""
+echo $MAPPING_SSL
 
 
+
 echo ""
-echo "Done!"
+echo " ------ All done - enjoy! ------ "
 echo ""
